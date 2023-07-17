c6e551a4759990eac14f0e02afce68e8224addd0
MahmoudZaqoutt <mahmoudzaqoutt12333@gmail.com>
2023-07-17 15:38:12 +0300

    error in paths

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,9 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
-import Test from "./test";
 
 function App() {
   return (
     <h1 className="text-3xl font-bold underline text-red-600 bg-black ">
       Simple React Typescript Tailwind Sample
-      <Test />
     </h1>
   );
 }
